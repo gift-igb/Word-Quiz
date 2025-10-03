a2eddfa21e538f5623dfb73cea8509221d4d6a13
Gift <gift17iw@gmail.com>
2025-10-02 18:04:00 -0700

    chore: snapshot current card setup

---
+++ components/Card.tsx
@@ -0,0 +1,24 @@
+﻿"use client"
+
+import styles from "@/app/Card.module.css"
+import { useState } from "react"
+type Props = {
+    En:String
+    Ja:String
+    numPicked:number
+    index:number
+    handleNumPicked: ()=> void
+}
+const Card = ({En, Ja, numPicked, index, handleNumPicked}:Props) => {
+
+    
+    
+    return (
+        
+        <div className={`${styles.card} ${numPicked === index ? styles.flipped : ""}`} onClick={numPicked === -1 ? handleNumPicked:()=>{}}>
+            <div className={`${styles.upside} ${numPicked === index ? styles.flippedUp : ""}`}></div>
+            <div className={`${styles.downside} ${numPicked === index ? styles.flippedDown : ""}`}></div>
+        </div>
+    )
+}
+export default Card;
